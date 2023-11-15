78f84d04f3516fe1b2a46de80f12e9f5f9e57b0d
Priyanshu0007 <63500076+Priyanshu0007@users.noreply.github.com>
2023-11-16 02:04:50 +0530

    image uploading added

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,12 +2,27 @@ const express =require('express');
 const Product = require('../models/product');
 const mongoose = require('mongoose');
 const router=express.Router();
+const multer = require('multer');
+const storage =multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"./uploads/");
+    },filename:function(req,file,cb){
+        cb(null,new Date().toISOString()+file.originalname);
+    }
+})
+const fileFilter=(req,file,cb)=>{
+    if(file.mimetype==='image/jpeg'||file.mimetype==='image/png'){ cb(null,true);}
+    else{cb(null,false);}
+}
+const upload=multer({storage:storage,limits:{fileSize:1024*1024*5},fileFilter:fileFilter})
 router.get('/',(req,res,next)=>{
-    Product.find().select("name price _id").exec().then(docs=>{
+    Product.find().select("name price _id productImage").exec().then(docs=>{
+        console.log(docs);
         const response={count:docs.length,products:docs.map(doc=>{
             return{
                 name:doc.name,
                 price:doc.price,
+                productImage:doc.productImage,
                 _id:doc._id,
                 request:{
                     type:"GET",
@@ -22,17 +37,17 @@ router.get('/',(req,res,next)=>{
         res.status(500).json({error:err})
     })
 })
-router.post('/',(req,res,next)=>{
-    const product=new Product({_id:new mongoose.Types.ObjectId(),name:req.body.name,price:req.body.price})
+router.post('/',upload.single('productImage'),(req,res,next)=>{
+    const product=new Product({_id:new mongoose.Types.ObjectId(),name:req.body.name,price:req.body.price,productImage:req.file.path,})
     product.save()
     .then(result=>{
-        console.log(result)
         res.status(201).json({
             message:"Created product successfully",
             createdProduct:{
                 name:result.name,
                 price:result.price,
                 _id:result._id,
+                productImage:req.file.path,
                 request:{
                     type:"GET",
                     url:"https://localhost:3000/"+result._id
@@ -49,7 +64,7 @@ router.post('/',(req,res,next)=>{
 })
 router.get('/:productId',(req,res,next)=>{
     const id=req.params.productId;
-    Product.findById(id).select("name price _id").exec().then(doc=>{
+    Product.findById(id).select("name price _id productImage").exec().then(doc=>{
         if(doc){res.status(200).json({product:doc,request:{type:"GET",description:"Get all products",url:"http://localhost/product"}})}
         else{res.status(404).json({message:"No valid entry for provided ID"})}
     })
--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongoose.Promise=global.Promise;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'))
+app.use("/uploads",express.static("uploads"))
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
