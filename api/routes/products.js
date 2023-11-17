eae11357ebead0d2e19372053edca5af72d103a5
Priyanshu0007 <63500076+Priyanshu0007@users.noreply.github.com>
2023-11-17 21:21:02 +0530

    log removed

--- api/routes/products.js
+++ api/routes/products.js
@@ -33,7 +33,6 @@ router.get('/',(req,res,next)=>{
         res.status(200).json(response);
     })
     .catch(err=>{
-        console.log(err);
         res.status(500).json({error:err})
     })
 })
