0bc30686e09e65f060a5ab80e5ce8d13ae222dd9
Priyanshu0007 <63500076+Priyanshu0007@users.noreply.github.com>
2023-11-18 15:13:16 +0530

    JWT auth added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app=express();
 const productRoutes=require('./api/routes/products')
 const orderRoutes=require('./api/routes/orders')
+const userRoutes=require('./api/routes/users')
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 mongoose.connect('mongodb+srv://Mrx:'+ process.env.MONGO_ATLAS_PASSWORD +'@cluster.sq7orxw.mongodb.net/?retryWrites=true&w=majority');
@@ -22,6 +23,7 @@ app.use((req,res,next)=>{
 })
 app.use('/products',productRoutes)
 app.use('/orders',orderRoutes)
+app.use('/users',userRoutes)
 app.use((req,res,next)=>{
     const error=new Error('Not found');
     error.status=404;
